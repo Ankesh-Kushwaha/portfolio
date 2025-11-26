df095461b4ff99fe09560aaa722da32405881ad6
ankeshkushwaha <ankeshkush9651@gmail.com>
2025-11-26 09:57:08 +0530

    some change

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import React from 'react'
-import Header from './components/header'
+import Header from './components/Header'
 import Hero from './components/Hero'
 import  {Projects} from './components/Project'
 import { Skills } from './components/Skills'
